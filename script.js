8bcb64e1478ad847393500d441906d9f281e9851
Alex Chon <achon@uwaterloo.ca>
2024-06-21 16:35:40 +0900

    Color Buttons Commit

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const inputGridSize = document.querySelector('#input-grid');
 const submitButton = document.querySelector('.submit-button');
 const clearButton = document.querySelector('.clear-button');
 const grid = document.querySelector('.grid-container');
-
+const blackButton = document.querySelector('#black-grid');
+const redButton = document.querySelector('#red-grid');
+const blueButton = document.querySelector('#blue-grid');
+const randomButton = document.querySelector('#random-grid');
+const eraserButton = document.querySelector('#eraser');
+let color = 'rgb(55,55,55)';
 
 submitButton.addEventListener('click', () => {
     if (inputGridSize.value <= 9) {
@@ -25,6 +30,26 @@ clearButton.addEventListener('click', (event) => {
     event.preventDefault();
     clearGrid();
 });
+
+blackButton.addEventListener('click', () => {
+    color = 'rgb(55,55,55)';
+});
+
+redButton.addEventListener('click', () => {
+    color = 'red';
+});
+
+blueButton.addEventListener('click', () => {
+    color = 'blue';
+});
+
+randomButton.addEventListener('click', () => {
+    color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+})
+
+eraserButton.addEventListener('click', () => {
+    color = 'white';
+})
 
 
 inputGridSize.addEventListener('keydown', (event) => {
@@ -61,13 +86,13 @@ function createGrid(size) {
 
 function colorDiv() {
     this.classList.add("grid-hover");
-    this.style.background = 'black';
 };
+
 
 function deactivateGridElement(event) {
     if (event.propertyName == "transform") {
         this.classList.remove("grid-hover");
-        this.style.background = 'rgb(55,55,55)';
+        this.style.background = `${color}`;
     }
 }
 
